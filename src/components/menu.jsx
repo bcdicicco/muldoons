7a95ebd02ca29cd2ea8c041208c13d91e43dc9a8
bdicicco <31937421+bcdicicco@users.noreply.github.com>
2020-06-21 03:12:29 -0400

    Stylized menu to be a carousel. Customized visuals

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,18 +4,31 @@ import appetizer from "../menuAppetizers.jpg";
 import dinners from "../menuDinners.jpg";
 import special from "../menuSpecialties.jpg";
 import toppings from "../menuToppings.jpg";
+import "../css/menu.css";
+import Carousel from "react-bootstrap/Carousel";
 
 class Menu extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1> Menu Page</h1>
-        <img src={cover}></img>
-        <img src={appetizer}></img>
-        <img src={dinners}></img>
-        <img src={special}></img>
-        <img src={toppings}></img>
+      <div className="overrides">
+        <Carousel interval={null} fade={true}>
+          <Carousel.Item>
+            <img className="d-block w-50" src={cover} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-50" src={appetizer} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-50" src={dinners} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-50" src={special} alt="" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-50" src={toppings} alt="" />
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
