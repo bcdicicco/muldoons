42a431cb8a577520ec967c315cb465c4741beb98
bdicicco <31937421+bcdicicco@users.noreply.github.com>
2020-06-22 04:48:20 -0400

    Started on gallery page. Cards for different menu items.

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import bbq from "../bbqChicken.jpg";
+import meat from "../meatPizza.jpg";
+import "../css/gallery.css";
 
 class Gallery extends Component {
   state = {};
@@ -6,6 +11,36 @@ class Gallery extends Component {
     return (
       <div>
         <h1> Gallery Page </h1>
+
+        <CardDeck id="Deck1">
+          <Card border="primary">
+            <Card.Img variant="top" src={bbq} />
+            <Card.Body>
+              <Card.Title>BBQ Chicken Pizza</Card.Title>
+              <Card.Text>
+                Toppings: Chicken, Red Onions, Green Peppers, BBQ Sauce
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card border="primary">
+            <Card.Img variant="top" src={bbq} />
+            <Card.Body>
+              <Card.Title>BBQ Chicken Pizza</Card.Title>
+              <Card.Text>
+                Toppings: Chicken, Red Onions, Green Peppers, BBQ Sauce
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card border="primary">
+            <Card.Img variant="top" src={bbq} />
+            <Card.Body>
+              <Card.Title>BBQ Chicken Pizza</Card.Title>
+              <Card.Text>
+                Toppings: Chicken, Red Onions, Green Peppers, BBQ Sauce
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
       </div>
     );
   }
